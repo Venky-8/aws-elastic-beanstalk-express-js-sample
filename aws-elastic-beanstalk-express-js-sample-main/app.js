708730640f0271644b6c756b044b62cc5c7820d0
Venkatesh Mangnale <venkatesh.mangnale@in.abb.com>
2025-07-25 15:58:54 +0530

    Second commit

--- aws-elastic-beanstalk-express-js-sample-main/app.js
+++ aws-elastic-beanstalk-express-js-sample-main/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to CI CD Demo!'));
+app.get('/', (req, res) => res.send('Welcome to CI CD Demo. Changes successfully implemented and propagated!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
